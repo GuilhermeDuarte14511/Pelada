fc4bc88646fdab18320fa0fcf892136b69527415
Guilherme Duarte <62967603+GuilhermeDuarte14511@users.noreply.github.com>
2025-03-07 19:56:11 -0300

    ajustes pwa e bootsrap

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,25 @@
 const CACHE_NAME = "pelada-cache-v1";
 const urlsToCache = [
-    "index.html",
-    "style.css",
-    "script.js",
-    "assets/bootstrap/css/bootstrap.min.css",
-    "assets/bootstrap/js/bootstrap.bundle.min.js",
-    "assets/FontAwesome/css/all.min.css",
-    "assets/FontAwesome/js/all.min.js",
-    "assets/icons/iconsPelada.jpg"
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./assets/bootstrap/css/bootstrap.min.css",
+    "./assets/bootstrap/js/bootstrap.bundle.min.js",
+    "./assets/FontAwesome/css/all.min.css",
+    "./assets/FontAwesome/js/all.min.js",
+    "./assets/icons/iconsPelada.jpg",
+    "./assets/icons/favicon.ico"
 ];
 
 // Instalar o Service Worker
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Caminhos para o cache:', urlsToCache);
+                return cache.addAll(urlsToCache);
+            })
+            .catch(error => console.error('Falha ao adicionar ao cache:', error))
     );
 });
 
@@ -25,6 +30,7 @@ self.addEventListener("activate", event => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheName !== CACHE_NAME) {
+                        console.log('Removendo cache antigo:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
@@ -36,7 +42,13 @@ self.addEventListener("activate", event => {
 // Buscar no Cache
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        caches.match(event.request).then(response => {
+            if (response) {
+                console.log('Servindo do cache:', event.request.url);
+                return response;
+            }
+            console.log('Buscando na rede:', event.request.url);
+            return fetch(event.request).catch(() => caches.match('./index.html'));
+        })
     );
 });
