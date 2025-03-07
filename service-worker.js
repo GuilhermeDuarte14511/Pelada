4d511aed87dd053fc8ce8681e8a39004206d12a8
Guilherme Duarte <62967603+GuilhermeDuarte14511@users.noreply.github.com>
2025-03-07 19:44:36 -0300

    ajustes icons e favicon

--- service-worker.js
+++ service-worker.js
@@ -7,21 +7,18 @@ const urlsToCache = [
     "assets/bootstrap/js/bootstrap.bundle.min.js",
     "assets/FontAwesome/css/all.min.css",
     "assets/FontAwesome/js/all.min.js",
-    "assets/icons/iconsPelada.jpg",
     "assets/icons/iconsPelada.jpg"
 ];
 
-// Instala o Service Worker e armazena os arquivos em cache
+// Instalar o Service Worker
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Ativa o Service Worker e remove caches antigos
+// Ativar o Service Worker
 self.addEventListener("activate", event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
@@ -36,12 +33,10 @@ self.addEventListener("activate", event => {
     );
 });
 
-// Intercepta as requisições e serve os arquivos do cache
+// Buscar no Cache
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+            .then(response => response || fetch(event.request))
     );
 });
